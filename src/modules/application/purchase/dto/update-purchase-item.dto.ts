2ad32483a562dddf4690a60805aa570419eab318
mdtajulislammt <mdtajulislam50599@gmail.com>
2025-09-03 16:26:24 +0600

    purchase model update

---
+++ src/modules/application/purchase/dto/update-purchase-item.dto.ts
@@ -0,0 +1,56 @@
+import { Type } from 'class-transformer';
+import { IsInt, IsNumber, IsOptional, IsString, Min } from 'class-validator';
+
+export class UpdatePurchaseItemDto {
+
+  @IsOptional() 
+  @IsString()
+  purchase_id?: string;
+
+  @IsOptional() 
+  @IsString()
+  line_id?: string;
+
+  @IsString()
+  item_id: string;
+
+  @IsOptional() 
+  @IsString() 
+  item_type_id?: string;
+
+  @IsOptional() 
+  @IsString() 
+  tax_id?: string;
+
+  @IsOptional() 
+  @IsString() 
+  itemCategory_id?: string;
+
+  @IsOptional() 
+  @IsString() 
+  unit_id?: string;
+
+  @Type(() => Number) 
+  @IsInt() 
+  @Min(1)
+  quantity: number;
+
+  @Type(() => Number) 
+  @IsNumber()
+  purchase_price: number;
+
+  @Type(() => Number) @IsNumber()
+  discount: number;
+
+  @IsOptional() 
+  @IsString() 
+  name?: string;
+
+  @IsOptional() 
+  @IsString() 
+  sku?: string;
+
+  @IsOptional() 
+  @IsString() 
+  description?: string;
+}
--- src/modules/application/purchase/dto/update-purchase.dto.ts
+++ src/modules/application/purchase/dto/update-purchase.dto.ts
@@ -1,4 +1,26 @@
-import { PartialType } from '@nestjs/swagger';
-import { CreatePurchaseDto } from './create-purchase.dto';
+import { Type } from 'class-transformer';
+import { IsArray, IsDateString, IsOptional, IsString, ValidateNested } from 'class-validator';
+import { UpdatePurchaseItemDto } from 'src/modules/application/purchase/dto/update-purchase-item.dto';
 
-export class UpdatePurchaseDto extends PartialType(CreatePurchaseDto) {}
+export class UpdatePurchaseDto {
+  @IsOptional() @IsString() account_type_id?: string | null;
+  @IsOptional() @IsString() vendor_id?: string | null;
+  @IsOptional() @IsString() billing_type_id?: string | null;
+  @IsOptional() @IsString() category_id?: string | null;
+
+  @IsOptional() @IsDateString()
+  purchase_date?: string | null;
+
+
+  @IsOptional()
+  @IsArray()
+  @ValidateNested({ each: true })
+  @Type(() => UpdatePurchaseItemDto)
+  items?: UpdatePurchaseItemDto[];
+
+
+  @IsOptional()
+  @IsArray()
+  @IsString({ each: true })
+  delete_line_ids?: string[];
+}
