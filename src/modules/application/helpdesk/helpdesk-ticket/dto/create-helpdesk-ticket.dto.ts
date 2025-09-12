58ba487979e2e18a7c84c49210e9bc9d32985533
barshon <barshon677@gmail.com>
2025-09-12 08:51:44 +0600

    update code

--- src/modules/application/helpdesk/helpdesk-ticket/dto/create-helpdesk-ticket.dto.ts
+++ src/modules/application/helpdesk/helpdesk-ticket/dto/create-helpdesk-ticket.dto.ts
@@ -1,34 +1,32 @@
-import { IsString, IsNotEmpty, IsOptional, IsEnum, IsEmail, IsUUID, IsObject, IsArray } from 'class-validator';
+import { IsString, IsNotEmpty, IsOptional, IsEnum, IsEmail } from 'class-validator';
 import { T_status } from '@prisma/client';  // Enum for ticket status
 import { Type } from 'class-transformer';
 
 export class CreateTicketDto {
-  @IsString()
-  @IsNotEmpty()
-  categoryId: string;  // Category of the HelpDeskTicket
+    @IsString()
+    @IsNotEmpty()
+    categoryId: string;  // Category of the HelpDeskTicket
 
-  @IsEnum(T_status)
-  status: T_status;  // Status of the ticket (OPEN, IN_PROGRESS, RESOLVED, CLOSED)
+    @IsEnum(T_status)
+    status: T_status;  // Status of the ticket (OPEN, IN_PROGRESS, RESOLVED, CLOSED)
 
-  @IsString()
-  @IsNotEmpty()
-  subject: string;  // Subject of the HelpDeskTicket
+    @IsString()
+    @IsNotEmpty()
+    subject: string;  // Subject of the HelpDeskTicket
 
-  @IsString()
-  @IsNotEmpty()
-  description: string;  // Description of the HelpDeskTicket
+    @IsString()
+    @IsNotEmpty()
+    description: string;  // Description of the HelpDeskTicket
 
-  @IsOptional()  // Optional for admins only
-  @IsUUID()
-  customerId?: string;  // Customer's user ID, required for SUPERADMIN
+    @IsOptional()  // Optional for admins only
+    customerId?: string;  // Customer's user ID, required for SUPERADMIN
 
-  @IsOptional()  // Optional for admins only
-  @IsEmail()
-  email?: string;  // Customer's email, required for SUPERADMIN
+    @IsOptional()  // Optional for admins only
+    @IsEmail()
+    email?: string;  // Customer's email, required for SUPERADMIN
 
-  // Constructor to type transform any incoming data
-  @Type(() => String)
-  @IsString()
-  @IsNotEmpty()
-  createdBy: string;  // Created by (user ID) from JWT
+    // Constructor to type transform any incoming data
+    @Type(() => String)
+    // @IsString()
+    createdBy: string;  // Created by (user ID) from JWT
 }
