7f0d1ed6a54a48efc64d7567507f8f7ddccf5662
Barshono96 <skadari172096@bscse.uiu.ac.bd>
2025-09-29 08:50:30 +0600

    code update

--- src/modules/application/application.module.ts
+++ src/modules/application/application.module.ts
@@ -33,15 +33,9 @@ import { BankaccountModule } from './accounting/bankaccount/bankaccount.module';
 import { ChartofaccountsModule } from './accounting/chartofaccounts/chartofaccounts.module';
 import { BasicPlanModule } from './subscription/basic-plan/basic-plan.module';
 import { CouponModule } from './subscription/coupon/coupon.module';
-<<<<<<< HEAD
 import { CompanySettingsModule } from './settings/company-settings/company-settings.module';
 @Module({
   imports: [NotificationModule, ContactModule, FaqModule, CustomerModule, VendorModule, LeadModule, DealModule, CrmReportModule, PipelinesModule, LeadStagesModule, DealStageModule, WorkspaceModule, SourcesModule, ItemsModule, CategoryModule, PurchaseModule, TasksModule, EmailtextModule, CallModule, LeadsUserModule, DiscussionModule, ActivityModule, LogTimeModule, InvoiceModule, HelpDeskCategoryModule, StockModule, HelpDeskTicketModule, SubscriptionSettingsModule, ModulePriceModule, ComboPlanModule, BankaccountModule, ChartofaccountsModule, BasicPlanModule, CouponModule, CompanySettingsModule],
-=======
-import { ChartofaccountModule } from './chartofaccount/chartofaccount.module';
-@Module({
-  imports: [NotificationModule, ContactModule, FaqModule, CustomerModule, VendorModule, LeadModule, DealModule, CrmReportModule, PipelinesModule, LeadStagesModule, DealStageModule, WorkspaceModule, SourcesModule, ItemsModule, CategoryModule, PurchaseModule, TasksModule, EmailtextModule, CallModule, LeadsUserModule, DiscussionModule, ActivityModule, LogTimeModule, InvoiceModule, HelpDeskCategoryModule, StockModule, HelpDeskTicketModule, SubscriptionSettingsModule, ModulePriceModule, ComboPlanModule, BankaccountModule, ChartofaccountsModule, BasicPlanModule, CouponModule, ChartofaccountModule],
->>>>>>> e32672334111ae672204bd8b95193cab9a62ed18
 
 })
 export class ApplicationModule { }
